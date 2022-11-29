4e787bbebbba2884212e4f24d6125b95aab67f10
iago-costa <univercomput@gmail.com>
2022-11-29 20:43:43 -0300

    restaurant.ts file to create the restaurant class and the functions to read the CSV file and return the filtered data in JSON format

---
+++ api/model/restaurant.ts
@@ -0,0 +1,95 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from 'csv-parse';
+import { finished } from "stream/promises";
+
+type Restaurant = {
+    id: number;
+    RestaurantName: string;
+    OpenHours: string;
+}
+
+class Restaurants {
+    // Properties
+    // fileName: string; // Name of the CSV file
+    // timeForVerification: number; // Time for verification
+    fileName: string;
+    timeForVerification: number;
+
+    constructor(fileName: string, timeForVerification: number) {
+        this.fileName = fileName;
+        this.timeForVerification = timeForVerification;
+    }
+
+    returnOpenRestaurants(data: Restaurant[]): Restaurant[] {
+        // Filter the data to return only the restaurants that are open at the time of verification
+        // Return the filtered data as an array of objects with the following structure:
+        // [
+        //     {
+        //         id: 1,
+        //         RestaurantName: 'Restaurant 1',
+        //         OpenHours: '10:00-22:00'
+        //     },
+        // ]
+        let open_restaurants: Restaurant[] = [];
+
+        for (let restaurant of data) {
+            if (restaurant.id !== 0) {
+                if (this.timeForVerification === 99999) {
+                    open_restaurants.push(restaurant);
+                }else{
+                    let openHours = restaurant.OpenHours.split('-');
+                    let openHour = parseInt(openHours[0].replace(':', ''));
+                    let closeHour = parseInt(openHours[1].replace(':', ''));
+                    if (this.timeForVerification >= openHour && this.timeForVerification <= closeHour) {
+                        open_restaurants.push(restaurant);
+                    }
+                }
+            }
+
+        }
+
+        return open_restaurants;
+    }
+
+    async readCSV(): Promise<Restaurant[]> {
+        // Read the CSV file and return the data as an array of objects with the following structure:
+        // [
+        //     {
+        //         id: 1,
+        //         RestaurantName: 'Restaurant 1',
+        //         OpenHours: '10:00-22:00'
+        //     },
+        // ]
+        const headers = ['RestaurantName', 'OpenHours'];
+        const csvFilePath = path.resolve(__dirname, this.fileName);
+
+        const processFile = async () => {
+            const records: any[] = [];
+            const parser = fs
+                .createReadStream(csvFilePath)
+                .pipe(parse({
+                    delimiter: ',',
+                    columns: headers
+                }));
+
+            parser.on('readable', function () {
+                let record;
+                while ((record = parser.read()) !== null) {
+                    record.id = records.length;
+                    records.push(record);
+                }
+            });
+
+            await finished(parser);
+            return records;
+        };
+
+        const records = await processFile();
+
+        return records
+    }
+}
+
+
+export { Restaurants, Restaurant };
